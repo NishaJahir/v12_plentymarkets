7337aa3821bd9f0c25e5cb79aec886d68f0d6318
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-09-26 11:21:06 +0530

    Update NovalnetInstalmentbyInvoicePaymentMethod.php

--- src/Methods/NovalnetInstalmentbyInvoicePaymentMethod.php
+++ src/Methods/NovalnetInstalmentbyInvoicePaymentMethod.php
@@ -193,17 +193,6 @@ class NovalnetInstalmentbyInvoicePaymentMethod extends PaymentMethodBaseService
     }
 
     /**
-     * Get name for the backend
-     *
-     * @param  string  $lang
-     * @return string
-     */
-    public function getBackendName(string $lang = 'de'):string
-    {
-        return $this->getName();
-    }
-
-    /**
      * Check if this payment method can handle subscriptions
      *
      * @return bool
